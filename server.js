7cc8e94d483e25474bffcf3a2780b9b1f3c781e4
ashishcodes4 <ashishcodes4@gmail.com>
2018-05-18 03:19:28 +0000

    adding ejs and testing route parameters

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get("/route/:thing", function(req, res){
+  var thing = req.params.thing;
+   res.render("route", {thingVar: thing});
+});
+
 app.listen("8080", () => {
     console.log("server listening on port 8080");
 });
